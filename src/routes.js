9f5d1272034b7b57646b47701c45e154f68fb9fe
Luis Moromisato <moromisato.luis@gmail.com>
2021-01-02 23:14:33 -0300

    added new login route

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const LoginController = require("./controllers/LoginController");
 const routes = express.Router();
 
 const MemberController = require('./controllers/MemberController');
 const TeamController = require("./controllers/TeamController");
 
+routes.post('/login', LoginController.login)
 routes.get('/member', MemberController.index)
 routes.post('/member', MemberController.create)
 routes.delete('/member/:id', MemberController.delete)
